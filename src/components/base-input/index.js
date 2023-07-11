5ab978bb535a560b4d1fbd9c1170db111e6ce84e
Необутов Дмитрий <doneobu1@mts.ru>
2023-07-11 18:57:13 +0300

    test push

--- src/components/base-input/index.js
+++ src/components/base-input/index.js
@@ -3,7 +3,7 @@ import Handlebars from "handlebars";
 const template = `
 <label class="input">
   <span class="input__label">{{ label }}</span>
-  <input class="input__input" type="{{ type }}">
+  <input class="input__input" type="">
 </label>
 `
 export const showBaseInput = ({label, type}) => Handlebars.compile(template)({label, type});
